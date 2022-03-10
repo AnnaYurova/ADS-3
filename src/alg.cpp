1f93b954153b049c887dd5422db7e2597273bdcd
AnnaYurova <94358663+AnnaYurova@users.noreply.github.com>
2022-03-10 12:19:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,41 +1,37 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-    int i = 0, j = size - 1; 
+    int i = 0, j = size - 1;
     int ab1 = 0;
     int ab2 = 0;
-    
     if (arr[j] == value) {
       ab1 = j+1;
   }
     if (arr[i] == value) {
       ab2 = i;
-  }
-  
-    while(i < j) {
+    }
+    while (i < j) {
         int ser = i + (j - i) / 2;
-        if(arr[ser] == value && arr[ser + 1] != value) {
-          ab1 = ser + 1 ;
+        if (arr[ser] == value && arr[ser + 1] != value) {
+          ab1 = ser + 1;
           break;
-        }else if (arr[ser] <= value) {
+        } else if (arr[ser] <= value) {
             i = ser + 1;
-        }else{
+        } else {
             j = ser;
         }
-    }
-    
+    }   
  i = 0, j = size - 1;
-    while(i < j) {
+    while (i < j) {
         int ser = i + (j - i) / 2;
-        if(arr[ser] == value && arr[ser - 1] != value) {
+        if (arr[ser] == value && arr[ser - 1] != value) {
           ab2 = ser;
           break;
-        }else if (arr[ser] >= value) {
+        } else if (arr[ser] >= value) {
             j = ser;
-        }else{
+        } else {
             i = ser + 1;
         }
     }
 return ab1-ab2;
 }
-  
